9928939c49a4a231dbb3a0f77087032116734144
reaganragsdale <rayrags10@gmail.com>
2020-12-09 21:30:22 +0000

    created Fibonacci sequence for two single threads and one multi thread and compared outputs of time

--- main.cpp
+++ main.cpp
@@ -2,35 +2,55 @@
 #include <iostream>       // std::cout
 #include <thread>         // std::thread
 #include <chrono>
+#include <ctime>
 
-using namespace std;
-
-void foo() 
-{
-  for (int i=0; i < 10000; i++) {
-    cout << "foo(): " << i << "\n";
-     std::this_thread::sleep_for (std::chrono::seconds(1));
+long long fib(long long n) {
+  if (n < 2) {
+    return n;
+  } else {
+    return fib(n-1) + fib(n-2);
   }
 }
 
-void bar(int x)
-{
-    for (int i=0; i < 10000; i++) {
-      cout << "I'm in bar(" << x << "): " << i << "\n";
-     std::this_thread::sleep_for (std::chrono::seconds(1));
-  }
+int main() {
+  auto start_time = std::chrono::high_resolution_clock::now();
+  long long input = 39;
+  long long result = fib(input);
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto time = end_time - start_time;
+
+  auto start_time2 = std::chrono::high_resolution_clock::now();
+  long long input2 = 38;
+  long long result2 = fib(input2);
+  auto end_time2 = std::chrono::high_resolution_clock::now();
+  auto time2 = end_time2 - start_time2;
+
+  std::cout << "result = " << result << '\n';
+  std::cout << "fib(" << input << ") took " <<
+  time/std::chrono::milliseconds(1) << "ms to run.\n";
+
+  std::cout << "\nresult2 = " << result2 << '\n';
+  std::cout << "fib(" << input2 << ") took " <<
+  time2/std::chrono::milliseconds(1) << "ms to run.\n";
+
+
+
+  auto start_time3 = std::chrono::high_resolution_clock::now();
+  long long input3 = 39;
+  long long result3 = fib(input3);
+  long long input4 = 38;
+  long long result4 = fib(input4);
+  auto end_time3 = std::chrono::high_resolution_clock::now();
+  auto time3 = end_time3 - start_time3;
+  std::cout << "\nresult3+4 = " << result3 << " + " << result4 <<'\n';
+  std::cout << "fib(" << input3 << " + " << input4 <<") took " <<
+  time3/std::chrono::milliseconds(1) << "ms to run.\n";
+
+  auto finalTime1 = time/std::chrono::milliseconds(1) + time2/std::chrono::milliseconds(1);
+  auto finalTime2 = time3/std::chrono::milliseconds(1);
+  std::cout << "\nTwo single thread added time =" << finalTime1 << "\n"
+  << "multithread time = " << finalTime2 <<'\n';
+
 
 }
-
-int main() 
-{
-  thread first (foo);
-  thread second (bar,0);
-
-  first.join();
-  second.join();
-
-  cout << "foo and bar completed.\n";
-
-  return 0;
-}
+  
